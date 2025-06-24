c7bb2a8f9defd26c495c3dc9c0178d13ce417aa2
LQEC87 <73701602+LQEC87@users.noreply.github.com>
2025-06-24 11:31:45 +0900

    fix test.py to add main() and description

--- test.py
+++ test.py
@@ -1,36 +1,42 @@
 from Encryptions import AES,RSA
 from useful_functions import anyfunction as af
 
-# Encrypt Example
-AES.encryption.key_length = 32
+def main():
+    "examples of useful python modules"
+    # Encrypt Example
+    AES.encryption.key_length = 32
 
-data = "test text".encode("utf-8")
+    data = "test text".encode("utf-8")
 
-aeskey = AES.encryption.keygen()
-enc1 = AES.encryption(aeskey, data=data)
-enc1.encrypter()
+    aeskey = AES.encryption.keygen()
+    enc1 = AES.encryption(aeskey, data=data)
+    enc1.encrypter()
 
-ciphertext = enc1.ct
-initiationvector = enc1.iv
+    ciphertext = enc1.ct
+    initiationvector = enc1.iv
 
-private_key, public_key = RSA.encryption.eskeygen()
-enc2 = RSA.encryption(None, public_key, data=aeskey)
-enc2.encrypter()
+    private_key, public_key = RSA.encryption.eskeygen()
+    enc2 = RSA.encryption(None, public_key, data=aeskey)
+    enc2.encrypter()
 
-cipheredkey = enc2.ct
+    cipheredkey = enc2.ct
 
-enc3 = RSA.encryption(private_key, None, ciphertext=cipheredkey)
-enc3.decrypter()
+    enc3 = RSA.encryption(private_key, None, ciphertext=cipheredkey)
+    enc3.decrypter()
 
-decryptedkey = enc3.data
+    decryptedkey = enc3.data
 
-enc4 = AES.encryption(decryptedkey, initiationvector, ciphertext)
-enc4.decrypter()
+    enc4 = AES.encryption(decryptedkey, initiationvector, ciphertext)
+    enc4.decrypter()
 
-print(f"Hybrid Encryption> data:{enc4.data}")
+    print(f"Hybrid Encryption> data:{enc4.data}")
 
-# Lot of function is here
-tet3_3 = af.tetration(3, 3) # 3↑↑3 = 3↑3↑3 = 3^3^3
-print(f"{af.intlog10(tet3_3)=}")
-print(f"{af.UTF8num("a")=}")
-print(f"{af.get_bytes(42)=}")
+    # Lot of function is here
+    tet3_3 = af.tetration(3, 3) # 3↑↑3 = 3↑3↑3 = 3^3^3
+    print(f"{af.intlog10(tet3_3)=}")
+    print(f"{af.UTF8num("a")=}")
+    print(f"{af.get_bytes(42)=}")
+
+
+if __name__ == "__main__":
+    main()
